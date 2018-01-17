0240ca02b2c3b1dbde3db5591d7e77a5aa0b7339
Felipe Castillo <fcastillo.ec@gmail.com>
2018-01-16 23:08:49 -0500

    Add Media Key test on all OSs

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
 const { Application } = require('spectron');
 const assert = require('assert');
 const path = require('path');
+const robot = require('robotjs');
 const delay = require('timeout-as-promise');
 const helper = require('./helper.js');
 
@@ -37,6 +38,50 @@ describe('application', function () {
     .waitUntilWindowLoaded().getWindowCount()
     .then(count => assert.equal(count, 2, 'Wrong number of windows'))
     .then(() => delay(2000)) // Inserts a delay so player window can minimize
+    .click('.pt-control-indicator') // Agrees to Youtuve Terms of Service
+    .click('.pt-button') // Continue button
     .getMainProcessLogs()
     .then(logs => helper.printLogs(logs)));
+
+  // Test requiered so media keys can produce an output
+  it('play song', () => app.client
+    .click('.album-art')
+    .waitForExist('.album-art')
+    .click('.album-art')
+    .waitForExist('.track-info')
+    .getMainProcessLogs()
+    .then((logs) => {
+      helper.printLogs(logs);
+      return assert(helper.isElement(logs, 'loadVideoById'), 'Couldn\'t play song');
+    }));
+
+  it('send play-pause key', () => Promise.resolve()
+    .then(() => robot.keyTap('audio_play'))
+    .then(() => delay(500))
+    .then(() => app.client.getMainProcessLogs())
+    .then((logs) => {
+      helper.printLogs(logs);
+      assert(helper.isElement(logs, 'Executing \'play-pause\' media key'), 'Media key was not received');
+      return assert(helper.isElement(logs, 'pauseVideo\', null'), 'Status didn\'t change');
+    }));
+
+  it('send play-next key', () => Promise.resolve()
+    .then(() => robot.keyTap('audio_next'))
+    .then(() => delay(500))
+    .then(() => app.client.getMainProcessLogs())
+    .then((logs) => {
+      helper.printLogs(logs);
+      assert(helper.isElement(logs, 'Executing \'play-next\' media key'), 'Media key was not received');
+      return assert(helper.isElement(logs, 'loadVideoById'), 'Status didn\'t change');
+    }));
+
+  it('send play-previous key', () => Promise.resolve()
+    .then(() => robot.keyTap('audio_prev'))
+    .then(() => delay(500))
+    .then(() => app.client.getMainProcessLogs())
+    .then((logs) => {
+      helper.printLogs(logs);
+      assert(helper.isElement(logs, 'Executing \'play-previous\' media key'), 'Media key was not received');
+      return assert(helper.isElement(logs, 'loadVideoById'), 'Status didn\'t change');
+    }));
 });
--- test/helper.js
+++ test/helper.js
@@ -1,25 +1,36 @@
-// Filters and formats logs belonging only to Headset.
-// Returns 3xN array, where N is the number of lines relevant to DEBUG=headset*
+// Formats logs and returns 3xN array, where N is the number of lines in the log
 function formatLogs(logs) {
   return logs
     .map((line) => {
       line = line.split(' ');
       return [line[0], line[1], line.splice(2).join(' ')];
-    })
-    .filter(line => !Number.isNaN(Date.parse(line[0])));
+    });
 }
 
-// Prints a color output of a 3xN array of logs
+// Prints a color output of the formatted logs, preserving structure as in DEBUG module
 function printFormatedLogs(logs) {
+  let previous = null;
   logs.forEach((log) => {
-    console.log('\t\x1b[33m%s \x1b[34m%s\x1b[0m', log[0], log[1], log[2]);
+    if (log[0] && log[1]) {
+      if (!Number.isNaN(Date.parse(log[0]))) {
+        console.log('\t\x1b[33m%s \x1b[34m%s\x1b[0m', log[0], log[1], log[2]);
+        previous = log;
+      } else { console.log('\t\x1b[31m%s\x1b[0m', log); }
+    } else if (previous) {
+      console.log('\t\x1b[33m%s \x1b[34m%s\x1b[0m', previous[0], previous[1], log[2]);
+    }
   });
 }
 
+// Prints all the returned logs with some formatting.
 function printLogs(logs) {
   printFormatedLogs(formatLogs(logs));
 }
 
-exports.printFormatedLogs = printFormatedLogs;
-exports.formatLogs = formatLogs;
+// Finds the string 'element' on the logs array
+function isElement(logs, element) {
+  return formatLogs(logs).some(line => line[2].includes(element));
+}
+
 exports.printLogs = printLogs;
+exports.isElement = isElement;
